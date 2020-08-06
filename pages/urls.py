2769e074839453511bd6463f8e70584d0668acb9
= <=mijeanot@protonmail.com>
2020-08-05 20:35:41 -0400

    added base.html and changed template DIR

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import about, home
+from .views import about, home, index
 
 urlpatterns = [
-    path("", home, name="home"),
+    path("", index, name="index"),
     path("about/", about, name="about"),
+    path("pages/", home, name="home"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    thispage = "index page"
+    return render(request, "index.html", {})
+
+
 def home(request):
     thispage = "home page"
     return render(request, "home.html", {"name": thispage})
